0002211b2154e20c7a278960efa62f5d2fe86e85
slawomir01krupa <slawomir01krupa@gmail.com>
2014-09-01 15:13:18 +0100

    first commit

---
+++ SimpleTaskSystem.WebApi/SimpleTaskSystemWebApiModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Abp.Dependency;
+using Abp.Modules;
+using Abp.Startup;
+using Abp.Startup.Application;
+using Abp.Startup.Web;
+using Abp.WebApi.Startup;
+
+namespace SimpleTaskSystem
+{
+    public class SimpleTaskSystemWebApiModule : AbpModule
+    {
+        public override Type[] GetDependedModules()
+        {
+            return new[]
+                   {
+                       typeof(AbpApplicationModule),
+                       typeof(AbpWebModule),
+                       typeof(AbpWebApiModule),
+                       typeof(SimpleTaskSystemApplicationModule)
+                   };
+        }
+
+        public override void Initialize(IAbpInitializationContext initializationContext)
+        {
+            base.Initialize(initializationContext);
+            IocManager.Instance.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
